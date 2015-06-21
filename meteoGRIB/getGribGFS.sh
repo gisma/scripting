b8b2a736cf313467ca75707fbc26669a279706ea
gisma <giswerk@gis-ma.org>
2015-06-22 01:08:13 +0200

    add check range

--- meteoGRIB/getGribGFS.sh
+++ meteoGRIB/getGribGFS.sh
@@ -18,22 +18,23 @@ if [[ "$1" == '-h' ]]; then
     echo " It is strongly recommended to extract a ROI"
     echo " "
     echo "Usage: ./getGribGFS.sh -h gives this brief help "
-    echo "       ./getGribGFS.sh -d=YYYYMMDD -wl=DDD.DD -el=DDD.DD -nl=DDD.DD -sl=DDD.DD -st=0 -et=96 -nc=FALSE -g1=FALSE"
+    echo "       ./getGribGFS.sh -d=YYYYMMDD -r=00 -wl=DDD.DD -el=DDD.DD -nl=DDD.DD -sl=DDD.DD -st=0 -et=96 -nc=FALSE -g1=FALSE"
     echo""
-    echo "-d=YYYYMMDD :  Date i.e. 20150621"
-    echo " -wl=DDD.DD :  western  Longitude of area to extract"
-    echo " -el=DDD.DD :  eastern  Longitude of area to extract"
-    echo " -sl=DDD.DD :  southern Latitude of area to extract"
-    echo " -nl=DDD.DD :  northern Latitude of area to extract"
-    echo "               all in decimal degrees"
+    echo " -d=YYYYMMDD :  date i.e. 20150621"
+    echo " -r=HH       :  hour of run [00 06 12 18]"
+    echo " -wl=DDD.DD  :  western  Longitude of area to extract"
+    echo " -el=DDD.DD  :  eastern  Longitude of area to extract"
+    echo " -sl=DDD.DD  :  southern Latitude of area to extract"
+    echo " -nl=DDD.DD  :  northern Latitude of area to extract"
+    echo "                all in decimal degrees"
     echo "                default lon [0 360], lat [-90 90]"
-    echo " -st=NN     :  start time [0] of forecast"
-    echo " -et=NN     :  end time [96]  of forecast"
-    echo "               0.25 GFS 3h => 96=10 days "
-    echo " -nc=boolean:  <FALSE> convert data to netcdf format"
-    echo " -g1=boolean:  <FALSE> convert data to grib1 format"
+    echo " -st=HH      :  start time [0] of forecast"
+    echo " -et=HH      :  end time [96]  of forecast"
+    echo "                0.25 GFS 3h => 96=10 days "
+    echo " -nc=boolean :  <FALSE> convert data to netcdf format"
+    echo " -g1=boolean :  <FALSE> convert data to grib1 format"
     echo " "
-    echo " example   :  ./getGribGFS.sh -d=20150620 -wl=-10.5 -el=20.25 -nl=60.75 -sl=40.0"
+    echo " example     :  ./getGribGFS.sh -d=20150620 -r=00 -wl=-10.5 -el=20.25 -nl=60.75 -sl=40.0"
     exit 0
 fi
 
@@ -42,6 +43,9 @@ do
 case $i in
     -d=*|--date=*)
     date="${i#*=}"
+    ;;
+    -r=*|--date=*)
+    run="${i#*=}"
     ;;
     -wl=*)
     leftlon="${i#*=}"
@@ -79,6 +83,7 @@ done
 
 # define default variables
 if [[ "${date}" == "" ]]      ; then  param=FALSE; date=`date -u +%Y%m%d`; fi
+if [[ "${run}" == "" ]] && [[ "${run}" != "00" ]] && [[ "${run}" && "06" ]] && [[ "${run}" != "12" ]] && [[ "${run}" != "18" ]]  ; then  param=FALSE; run="00"; fi
 if [[ "${StartTime}" == "" ]] ; then  param=FALSE; StartTime=0 ; fi
 if [[ "${EndTime}" == ""   ]] ; then  param=FALSE; EndTime=96 ; fi
 if [[ "${leftlon}" == ""   ]] ; then  param=FALSE; leftlon="0" ; fi
@@ -89,6 +94,7 @@ if [[ "${netCDF}" == ""    ]] ; then  param=FALSE; netCDF="FALSE" ; fi
 if [[ "${grib1}" == "" ]]     ; then  param=FALSE; grib1="FALSE" ; fi
 echo "extracting the data with the following parameters:"
 echo date=${date}
+echo run=${run}
 echo StartTime=${StartTime}
 echo EndTime=${EndTime}
 echo leftlon=${leftlon}
@@ -112,7 +118,7 @@ filename=$(basename "$INFILE")
 filename="${filename%.*}"
 fndate=${filename:20:8}
 INDATAPATH=$(dirname "$INFILE")
-EXDATAPATH=/home/$USER/$DATADIR/$MODELDIR/$date
+EXDATAPATH=/home/$USER/$DATADIR/$MODELDIR/$date/$run
 SCRIPTPATH=/home/$USER/dev/scripts/meteoGRIB
 echo "~~~~~~~~~~~~~~~~~"
 echo "output folder:"
@@ -128,7 +134,6 @@ if [ ! -d $EXDATAPATH ]; then mkdir -p ${EXDATAPATH} ; fi
 cd $EXDATAPATH
 
 # fix the substitution variables to fixed format 3 numbers
-charStartTime=$(printf "%02d\n" $StartTime)
 charNewHour=$(printf "%03d\n" $StartTime)
 hour=$StartTime
 
@@ -136,11 +141,11 @@ hour=$StartTime
 # we can also use the regular download from gribmaster  or similar scripts
 # next line you will find a filter call currently we are loading all levels and all vars in a small subregion.
 # be careful each full earth all variables&levels file is about 550 MB and GRIB is a really good compression!
-#URL="http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${charStartTime}z.pgrb2.0p25.f${charNewHour}&lev_1000_mb=on&lev_100_mb=on&lev_10_m_above_ground=on&lev_200_mb=on&lev_2_m_above_ground=on&lev_300_mb=on&lev_400_mb=on&lev_500_mb=on&lev_600_mb=on&lev_700_mb=on&lev_750_mb=on&lev_800_mb=on&lev_850_mb=on&lev_900_mb=on&lev_925_mb=on&lev_950_mb=on&lev_975_mb=on&var_4LFTX=on&var_ALBDO=on&var_CAPE=on&var_CIN=on&var_CLWMR=on&var_CPRAT=on&var_CRAIN=on&var_CSNOW=on&var_CWAT=on&var_CWORK=on&var_DPT=on&var_GUST=on&var_HGT=on&var_POT=on&var_PRATE=on&var_PRES=on&var_RH=on&var_SUNSD=on&var_TMAX=on&var_TMIN=on&var_TMP=on&var_UGRD=on&var_VGRD=on&var_VVEL=on&subregion=&leftlon=${leftlon}&rightlon=${rightlon}&toplat=${toplat}&bottomlat=${bottomlat}&dir=%2Fgfs.${date}${charStartTime}"
+#URL="http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${run}z.pgrb2.0p25.f${charNewHour}&lev_1000_mb=on&lev_100_mb=on&lev_10_m_above_ground=on&lev_200_mb=on&lev_2_m_above_ground=on&lev_300_mb=on&lev_400_mb=on&lev_500_mb=on&lev_600_mb=on&lev_700_mb=on&lev_750_mb=on&lev_800_mb=on&lev_850_mb=on&lev_900_mb=on&lev_925_mb=on&lev_950_mb=on&lev_975_mb=on&var_4LFTX=on&var_ALBDO=on&var_CAPE=on&var_CIN=on&var_CLWMR=on&var_CPRAT=on&var_CRAIN=on&var_CSNOW=on&var_CWAT=on&var_CWORK=on&var_DPT=on&var_GUST=on&var_HGT=on&var_POT=on&var_PRATE=on&var_PRES=on&var_RH=on&var_SUNSD=on&var_TMAX=on&var_TMIN=on&var_TMP=on&var_UGRD=on&var_VGRD=on&var_VVEL=on&subregion=&leftlon=${leftlon}&rightlon=${rightlon}&toplat=${toplat}&bottomlat=${bottomlat}&dir=%2Fgfs.${date}${run}"
 # allvars and all levels in a subregion
 
 # initial setup the url
-URL="http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${charStartTime}z.pgrb2.0p25.f${charNewHour}&all_lev=on&all_var=on&subregion=&leftlon=${leftlon}&rightlon=${rightlon}&toplat=${toplat}&bottomlat=${bottomlat}&dir=%2Fgfs.${date}${charStartTime}"
+URL="http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${run}z.pgrb2.0p25.f${charNewHour}&all_lev=on&all_var=on&subregion=&leftlon=${leftlon}&rightlon=${rightlon}&toplat=${toplat}&bottomlat=${bottomlat}&dir=%2Fgfs.${date}${run}"
 
 # first we check if the file exist
 echo "check if data is available...."
@@ -150,10 +155,10 @@ if [[ `wget -S --spider $URL  2>&1 | grep 'HTTP/1.1 200 OK'` ]]
 	while [ $hour -le $EndTime ]
 	do
 		# loop setup the url
-    	URL="http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${charStartTime}z.pgrb2.0p25.f${charNewHour}&all_lev=on&all_var=on&subregion=&leftlon=${leftlon}&rightlon=${rightlon}&toplat=${toplat}&bottomlat=${bottomlat}&dir=%2Fgfs.${date}${charStartTime}"
+    	URL="http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${run}z.pgrb2.0p25.f${charNewHour}&all_lev=on&all_var=on&subregion=&leftlon=${leftlon}&rightlon=${rightlon}&toplat=${toplat}&bottomlat=${bottomlat}&dir=%2Fgfs.${date}${run}"
 		echo "start download...."
 		# then we get the data via cURL
-		curl "$URL" -o gfs.t${charStartTime}z.pgrb2.0p25.f${charNewHour}
+		curl "$URL" -o gfs.t${run}z.pgrb2.0p25.f${charNewHour}
 		# INCREMENT timeslot
 		hour=$(($hour + 3))
 		# correct time format for substitution
@@ -169,28 +174,28 @@ fi
 
 
 # rename initial analysis data due to a different number of variables
-mv gfs.t${charStartTime}z.pgrb2.0p25.f000 gfs.t${charStartTime}z.pgrb2.0p25.f000.grb
+mv gfs.t${run}z.pgrb2.0p25.f000 gfs.t${run}z.pgrb2.0p25.f000.grb
 
 # merge all grib2 files to one
-cdo mergetime gfs.t${charStartTime}z.pgrb2.0p25.f??? gfs.t${charStartTime}z.pgrb2.0p25.${date}_${charStartTime}_${EndTime}.grb
+cdo mergetime gfs.t${run}z.pgrb2.0p25.f??? gfs.t${run}z.pgrb2.0p25.${date}_${StartTime}_${EndTime}.grb
 
 # grib2 to netcdf
 if [[ "${netCDF1}" == "TRUE" ]] ; then
   # convert it to netcdf (it seems to work even if there some warnings)
-  cdo -f nc copy gfs.t${charStartTime}z.pgrb2.0p25.${date}_${charStartTime}_${EndTime}.grb gfs.t${charStartTime}z.pgrb2.0p25.${date}_${charStartTime}_${EndTime}.nc
+  cdo -f nc copy gfs.t${run}z.pgrb2.0p25.${date}_${StartTime}_${EndTime}.grb gfs.t${run}z.pgrb2.0p25.${date}_${StartTime}_${EndTime}.nc
   # and squeeze it via shuffeling and compressing (http://www.unidata.ucar.edu/blogs/developer/en/entry/netcdf_compression)
-  nccopy -u -d5 gfs.t${charStartTime}z.pgrb2.0p25.${date}_${charStartTime}_${EndTime}.nc gfs.t${charStartTime}z.0p25.${date}_${charStartTime}_${EndTime}.nc
+  nccopy -u -d5 gfs.t${run}z.pgrb2.0p25.${date}_${StartTime}_${EndTime}.nc gfs.t${run}z.0p25.${date}_${StartTime}_${EndTime}.nc
   # remove uncompressed file
-  if [ ! -f gfs.t${charStartTime}z.pgrb2.0p25.${date}_${charStartTime}_${EndTime}.nc ]; then rm -r gfs.t${charStartTime}z.pgrb2.0p25.${date}_${charStartTime}_${EndTime}.nc ; fi
+  if [ ! -f gfs.t${run}z.pgrb2.0p25.${date}_${StartTime}_${EndTime}.nc ]; then rm -r gfs.t${run}z.pgrb2.0p25.${date}_${StartTime}_${EndTime}.nc ; fi
 fi
 
 # grib2 to grib1
 if [[ "${grib1}" == "TRUE" ]] ; then
   # convert the grib 2 to grib one for usage with zygrib
-  cnvgrib -g21 ${date}/gfs.t${charStartTime}z.pgrb2.0p25.${date}_${charStartTime}_${EndTime}.grb ${date}/gfs.t${charStartTime}z.pgrb1.0p25.$Date_${charStartTime}_${EndTime}.grb
+  cnvgrib -g21 ${date}/gfs.t${run}z.pgrb2.0p25.${date}_${StartTime}_${EndTime}.grb ${date}/gfs.t${run}z.pgrb1.0p25.${date}_${StartTime}_${EndTime}.grb
 fi
 
 # remove single files
-rm -r gfs.t${charStartTime}z.pgrb2.0p25.f???
+rm -r gfs.t${run}z.pgrb2.0p25.f???
 
 echo "getGribGFS.sh finished"
